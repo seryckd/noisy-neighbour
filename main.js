41707665b95132f9c815765162628480df77045d
Darren on MBP <darrens@MBP.local>
2016-04-03 22:42:29 -0400

    Polyfil windows.performance.now for iPhone4

--- main.js
+++ main.js
@@ -3,6 +3,33 @@
 
 var NOISY = NOISY || {};
 
+// Polyfill for window.performance.now
+// (not available on iPhone 4)
+(function(){
+
+  if ("performance" in window == false) {
+      window.performance = {};
+  }
+  
+  Date.now = (Date.now || function () {  // thanks IE8
+	  return new Date().getTime();
+  });
+
+  if ("now" in window.performance == false){
+    
+    var nowOffset = Date.now();
+    
+    if (performance.timing && performance.timing.navigationStart){
+      nowOffset = performance.timing.navigationStart
+    }
+
+    window.performance.now = function now(){
+      return Date.now() - nowOffset;
+    }
+  }
+
+})();
+
 NOISY.selectedCell = null;
 
 // Called on canvas event listener
